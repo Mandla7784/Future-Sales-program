4b7c77002eb816c70cb5dc2da2602df058624b6b
root <156649157+Mandla7784@users.noreply.github.com>
2025-11-14 11:04:35 +0200

    Added connection function to postgess database

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import psycopg2
+
+def connect_db():
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            port=5432,
+            database="future_sales",
+            user="mandla",
+            password="secret123"
+        )
+        print("Connected to PostgreSQL successfully!")
+        return conn
+    except Exception as e:
+        print("Connection error:", e)
+
+if __name__ == "__main__":
+    connect_db()
+
